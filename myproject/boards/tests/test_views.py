5df37d57790af6088c456941d4c7da8252f8ff3a
Wirelessr <ifloveyou2014@gmail.com>
2017-11-28 12:51:47 +0000

    Add login/logout

--- myproject/boards/tests/test_views.py
+++ myproject/boards/tests/test_views.py
@@ -3,9 +3,9 @@ from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.contrib.auth.models import User
 
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
